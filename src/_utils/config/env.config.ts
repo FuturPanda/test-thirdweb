8e0806ada088f3775e2db0bbf722eaef7faaeb08
Hoareau.Steevy <68106917+FuturPanda@users.noreply.github.com>
2025-07-01 09:26:55 +0200

    add env

--- src/_utils/config/env.config.ts
+++ src/_utils/config/env.config.ts
@@ -1,37 +1,16 @@
-import { Logger } from '@nestjs/common';
-import { plainToInstance, Type } from 'class-transformer';
+import { Logger } from "@nestjs/common";
+import { plainToInstance, Type } from "class-transformer";
 import {
   IsNumber,
   IsString,
   ValidateNested,
   validateSync,
-} from 'class-validator';
-import { exit } from 'process';
+} from "class-validator";
+import { exit } from "process";
 
 export class ServerConfig {
   @IsNumber()
   PORT: number;
-}
-
-export class DatabaseConfig {
-  @IsString()
-  DATABASE_URL: string;
-
-  @IsString()
-  DATABASE_NAME: string;
-}
-
-export class OpenAI {
-  @IsString()
-  OPENAI_API_KEY: string;
-}
-
-export class MonadExplorerConfig {
-  @IsString()
-  MONAD_EXPLORER_API_KEY: string;
-
-  @IsString()
-  MONAD_EXPLORER_BASE_URL: string;
 }
 
 export class ThirdWebConfig {
@@ -57,20 +36,8 @@ export class EnvironmentVariables {
   SERVER: ServerConfig;
 
   @ValidateNested()
-  @Type(() => DatabaseConfig)
-  DATABASE: DatabaseConfig;
-
-  @ValidateNested()
-  @Type(() => OpenAI)
-  OPENAI: OpenAI;
-
-  @ValidateNested()
   @Type(() => ThirdWebConfig)
   THIRDWEB: ThirdWebConfig;
-
-  @ValidateNested()
-  @Type(() => MonadExplorerConfig)
-  MONADEXPLORER: MonadExplorerConfig;
 }
 
 export function validateEnv(config: Record<string, unknown>) {
@@ -78,23 +45,12 @@ export function validateEnv(config: Record<string, unknown>) {
     SERVER: {
       PORT: config.PORT,
     },
-    DATABASE: {
-      DATABASE_URL: config.DATABASE_URL,
-      DATABASE_NAME: config.DATABASE_NAME,
-    },
-    OPENAI: {
-      OPENAI_API_KEY: config.OPENAI_API_KEY,
-    },
     THIRDWEB: {
       THIRDWEB_SECRET_KEY: config.THIRDWEB_SECRET_KEY,
       THIRDWEB_DOMAIN: config.THIRDWEB_DOMAIN,
       THIRDWEB_PRIVATE_KEY: config.THIRDWEB_PRIVATE_KEY,
       THIRDWEB_ORE_CONTRACT_ADDRESS: config.THIRDWEB_ORE_CONTRACT_ADDRESS,
       THIRDWEB_USDC_CONTRACT_ADDRESS: config.THIRDWEB_USDC_CONTRACT_ADDRESS,
-    },
-    MONADEXPLORER: {
-      MONAD_EXPLORER_API_KEY: config.MONAD_EXPLORER_API_KEY,
-      MONAD_EXPLORER_BASE_URL: config.MONAD_EXPLORER_BASE_URL,
     },
   };
 
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,21 +1,19 @@
-import { forwardRef, Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UniqueExistsConstraint } from 'src/_utils/decorators/unique-exists.decorator';
-import { EncryptionModule } from 'src/encryption/encryption.module';
-import { TokensModule } from 'src/tokens/tokens.module';
-import { UserExistsRule } from './_utils/user-exist.rule';
-import { UsersController } from './users.controller';
-import { UsersMapper } from './users.mapper';
-import { UsersRepository } from './users.repository';
-import { User, UserSchema } from './users.schema';
-import { UsersService } from './users.service';
+import { forwardRef, Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { MongooseModule } from "@nestjs/mongoose";
+import { UniqueExistsConstraint } from "src/_utils/decorators/unique-exists.decorator";
+import { TokensModule } from "src/tokens/tokens.module";
+import { UserExistsRule } from "./_utils/user-exist.rule";
+import { UsersController } from "./users.controller";
+import { UsersMapper } from "./users.mapper";
+import { UsersRepository } from "./users.repository";
+import { User, UserSchema } from "./users.schema";
+import { UsersService } from "./users.service";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     forwardRef(() => JwtModule),
-    EncryptionModule,
     TokensModule,
   ],
   providers: [
